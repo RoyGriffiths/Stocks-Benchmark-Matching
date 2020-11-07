c82996b65b596b34c61a4d1d3decc09936823457
Roy Griffiths <roy.ah.griffiths@gmail.com>
2020-11-07 23:34:22 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,9 +7,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Main {
-    /**
-     * Iterate through each line of input.
-     */
     public static void main(String[] args) throws IOException {
         InputStreamReader reader = new InputStreamReader(System.in, StandardCharsets.UTF_8);
         BufferedReader in = new BufferedReader(reader);
